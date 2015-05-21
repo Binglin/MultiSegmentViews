265ca0fcc761def70224a5aab5de7fedd1093cf2
zhenglinqin <zheng.linqin@hongfu001.com>
2015-05-21 14:11:24 +0800

    Initial Commit

---
+++ MultiSegmentController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MultiSegmentController
+//
+//  Created by Zhenglinqin on 15/5/21.
+//  Copyright (c) 2015年 Binglin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MultiSegmentController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MultiSegmentController
+//
+//  Created by Zhenglinqin on 15/5/21.
+//  Copyright (c) 2015年 Binglin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
